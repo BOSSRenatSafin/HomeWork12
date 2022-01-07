0b0baaa6fa22173881dce24ddf39083e84841fa3
Ренат <BOSS.Renat.Safin@gmail.com>
2022-01-07 20:26:07 +0300

    Add to standart cell.

--- HomeWork12/Sources/Scenes/Sections/FirstSection.swift
+++ HomeWork12/Sources/Scenes/Sections/FirstSection.swift
@@ -6,3 +6,30 @@
 //
 
 import UIKit
+
+class FirstSection: UITableViewCell {
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Settings
+    
+    static func addCellsToSection() -> [[SettingsCell]] {
+        return [[SettingsCell(nameImage: "wifi",
+                              colorImage: .link,
+                              textLabel: "Wi-fi",
+                              typeCell: .cell,
+                              trailingLabelText: "Не подключен",
+                              notificationLabelText: nil
+                             )
+                ]]
+    }
+    
+}
