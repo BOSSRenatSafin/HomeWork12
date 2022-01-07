5d598a95b54df367317d1efa6c8cc6253fbb38ea
Ренат <BOSS.Renat.Safin@gmail.com>
2022-01-07 19:52:03 +0300

    Add to navigationBar.

--- HomeWork12/Sources/App/AppDelegate.swift
+++ HomeWork12/Sources/App/AppDelegate.swift
@@ -9,8 +9,15 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let navigationBar = UINavigationController(rootViewController: ViewController())
+        
+        window?.rootViewController = navigationBar
+        window?.makeKeyAndVisible()
         return true
     }
 }
