218fb9c059a245732797400740997c7c26f548a5
Ренат <BOSS.Renat.Safin@gmail.com>
2022-01-07 20:38:47 +0300

    Add cells and sections.

--- HomeWork12/Sources/Scenes/Cells/SwitchCell.swift
+++ HomeWork12/Sources/Scenes/Cells/SwitchCell.swift
@@ -6,3 +6,39 @@
 //
 
 import UIKit
+
+class SwitchCell: Cell {
+    
+    static let identifireSwitchCell = "SwitchCell"
+    
+    // MARK: - Views
+    
+    private lazy var toSwitch: UISwitch = {
+        let toSwitch = UISwitch()
+        toSwitch.onTintColor = .systemBlue
+        return toSwitch
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupHierarcyToOverride()
+        setupLayoutToOverride()    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Settings
+    
+    override func setupHierarcyToOverride() {
+        contentView.addSubview(toSwitch)
+    }
+    
+    override func setupLayoutToOverride() {
+        toSwitch.translatesAutoresizingMaskIntoConstraints = false
+        toSwitch.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+        toSwitch.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20).isActive = true
+    }
+}
